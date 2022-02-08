2cb81f9d4e97e6d2ee156cde23910f2466575683
Xodarap7 <basyar86@gmail.com>
2022-02-08 11:47:54 +0500

    python:functions exercise #9 solution

---
+++ Python:_Functions/exersises_code/9.py
@@ -0,0 +1,18 @@
+def make_module(step=1) -> dict:
+
+    return {
+        'inc': lambda x: x+step,
+        'dec': lambda x: x-step
+    }
+
+
+def main():
+    m = make_module()
+    m2 = make_module(step=2)
+    print(m['inc'](10))
+    print(m['dec'](20))
+    print(m2['inc'](1))
+
+
+if __name__ == '__main__':
+    main()
