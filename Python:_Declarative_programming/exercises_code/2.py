5acf9e799802de1f0d529ad0042cf6139831fecc
Xodarap7 <basyar86@gmail.com>
2022-02-15 10:51:10 +0500

    python:declarative_programming exercise #2 solution

---
+++ Python:_Declarative_programming/exercises_code/2.py
@@ -0,0 +1,72 @@
+from typing import List, Any
+
+NUMBERS_LISTS = List[Any]
+
+
+def odds_from_odds(elements: List[NUMBERS_LISTS]) -> List[NUMBERS_LISTS]:
+    """
+    Function changes the original element
+    """
+    result = []
+
+    for i, element in enumerate(elements):
+        new_element = []
+
+        if (i+1) % 2 == 1:
+
+            for j, number in enumerate(element):
+                if (j+1) % 2 == 1:
+                    new_element.append(number)
+            result.append(new_element)
+
+    return result
+
+
+def find_to_delete(elem):
+    """
+    Create list of index with elements to delete
+    """
+    to_delete = []
+
+    for i, _ in enumerate(elem):
+        if (i+1) % 2 == 0:
+            to_delete.append(i)
+
+    delete_items(to_delete, elem)
+
+
+def delete_items(to_delete, elem):
+    """
+    deleting an element by index
+    """
+    to_delete.reverse()
+
+    for item in to_delete:
+        elem.pop(item)
+
+
+def keep_odds_from_odds(elements: List[NUMBERS_LISTS]) -> None:
+    """
+    Function does not change the original element
+    """
+    find_to_delete(elements)
+
+    for element in elements:
+        find_to_delete(element)
+
+
+def main():
+    l = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [7, 8, 9]]
+    print(odds_from_odds(l))
+    print(l)
+    print(odds_from_odds([]))
+    print(odds_from_odds([[]]))
+
+    print(keep_odds_from_odds(l))
+    print(l)
+    print(keep_odds_from_odds([]))
+    print(keep_odds_from_odds([[]]))
+
+
+if __name__ == '__main__':
+    main()
